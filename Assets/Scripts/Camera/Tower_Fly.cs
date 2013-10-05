994e17e5b370406cc6149eb3bf4fc7629619c10b
Kirtanei <Bright_Failure@hotmail.com>
2013-10-05 13:04:23 +0200

    MainMenu Scene

---
+++ Assets/Scripts/Camera/Tower_Fly.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower_Fly : MonoBehaviour {
+	
+	public Transform oTower;
+	
+	public float fMaxHeight = 15f;
+	public float fMinHeight = 3f;
+	
+	public float fVelocity = 0.6f;
+	
+	private bool bRise = true;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (transform.position.y < fMinHeight)
+		{
+			bRise = true;
+		}
+		else if (transform.position.y > fMaxHeight)
+		{
+			bRise = false;
+		}
+		RaiseCamera(bRise);
+	} 
+	
+	
+	
+	void RaiseCamera(bool bRise)
+	{
+		if(bRise == true)
+		{
+			transform.Translate(Vector3.up * Time.deltaTime * fVelocity);
+			transform.RotateAround(oTower.position, (Vector3.up*10), 20*Time.deltaTime);
+		}
+		else if(bRise == false)
+		{
+			transform.Translate(-Vector3.up * Time.deltaTime * fVelocity);
+			transform.RotateAround(oTower.position, (Vector3.up*10), 20*Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/PressStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressStart : MonoBehaviour {
+	
+	public GUIText oStart;
+	public GUIText oContinue;
+	
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+	
+	// Loads the level when clicking the "PRESS START TO PLAY" text.
+	void OnMouseDown() 
+	{
+		//Loads up the level.
+		//Application.LoadLevel();
+		Debug.Log ("Start has been pressed!");
+	}
+}
